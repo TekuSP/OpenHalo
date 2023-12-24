00032d8b600c361cd5712d48f5effe250eb51d96
TekuSP <richard.torhan@windowslive.com>
2023-12-24 15:17:14 +0100

    Add config

---
+++ Configs/MainConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace OpenHalo.Configs
+{
+    [Serializable]
+    public class MainConfig
+    {
+        public string MoonrakerApiKey
+        {
+            get; set;
+        }
+        public string MoonrakerUri
+        {
+            get; set;
+        }
+        public Wifi[] Wifis
+        {
+            get; set;
+        }
+    }
+    [Serializable]
+    public class Wifi
+    {
+        public string SSID
+        {
+            get; set;
+        }
+        public string Password
+        {
+            get; set;
+        }
+        public bool Hidden
+        {
+            get; set;
+        }
+    }
+}
